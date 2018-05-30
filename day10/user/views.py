3a4afc7ced8ea21d05efa2eba76a322066eee5a5
leezhonglin <380604322@qq.com>
2018-05-30 08:18:56 +0800

    更新文件注释

--- day10/user/views.py
+++ day10/user/views.py
@@ -1,18 +1,15 @@
 import random
-
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-
 from django.core.urlresolvers import reverse
-
 from user.models import Users
 
 '''
 注册页面处理
 '''
+"""django默认的注册登录"""
 
 
 def djregister(request):
@@ -59,6 +56,12 @@ def djlogout(request):
         return HttpResponseRedirect(reverse('user:login'))
 
 
+#################################################
+"""
+自定义登录注册
+"""
+
+
 def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
@@ -74,10 +77,11 @@ def register(request):
     if pwd1 != pwd2:
         msg = '两次密码不一致'
         return render(request, 'register.html', {'msg': msg})
-
-    Users.objects.create(username=username,password=pwd1)
-
+    # 使用自定义的mdoels创建用户
+    Users.objects.create(username=username, password=pwd1)
+    # 成功返回用户登录页面
     return HttpResponseRedirect(reverse('user:login'))
+
 
 def login(request):
     if request.method == 'GET':
@@ -86,21 +90,20 @@ def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
-
-        user = Users.objects.filter(username=username,password=password).first()
+        # 查询数据库中是否有页面传过来的用户
+        user = Users.objects.filter(username=username, password=password).first()
         if user:
             # 先产生随机的字符串 ,长度为28
-            s ='qazwsxedcrfvtgbyhnujmikolp1234567890'
+            s = 'qazwsxedcrfvtgbyhnujmikolp1234567890!@#$%^&*()_+'
             ticket = ''
             for i in range(28):
                 ticket += random.choice(s)
-            #保存在服务端
+            # 保存在服务端
             user.ticket = ticket
             user.save()
-            #保存在客户端
+            # 保存在客户端
             response = HttpResponseRedirect(reverse('app:index'))
-            response.set_cookie('ticket',ticket)
+            response.set_cookie('ticket', ticket,max_age=129000)
             return response
         else:
             msg = '用户名或者密码错误'
@@ -110,6 +113,7 @@ def login(request):
 def logout(request):
     if request.method == 'GET':
         response = HttpResponseRedirect(reverse('user:login'))
+        # 删除响应中的ticket
         response.delete_cookie('ticket')
 
-        return  response
+        return response
